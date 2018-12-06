8a18cfd50753fdd164d4972275aa7bf5c38f2314
Chris Cable <ccable827@yahoo.com>
2018-12-06 20:02:41 +0000

    Automatically backed up by Learn

---
+++ index.js
@@ -0,0 +1,63 @@
+function shout(string) {
+  return string.toUpperCase()
+}
+
+function whisper(string) {
+  return string.toLowerCase()
+}
+
+
+function logShout(string) {
+  console.log(string.toUpperCase())
+}
+
+var string = logWhisper
+
+function logWhisper(string) {
+  console.log(string.toLowerCase())
+}
+
+var lowercase = sayHiToGrandma
+
+function sayHiToGrandma(string) {
+  string.toLowerCase() === lowercase
+  return "I can't hear you!";
+}
+
+function sayHiToGrandma(string) {
+  string.toLowerCase() === lowercase
+  return "I can't hear you!"
+}
+
+var uppercase = sayHiToGrandma
+
+function sayHiToGrandma(string) {
+  string.toUpperCase() === uppercase
+  return "YES INDEED!"
+}
+
+var lowercase = sayHiToGrandma
+
+function sayHiToGrandma(string) {
+  string.toLowerCase() === lowercase
+  return "I can't hear you!"
+}
+
+var uppercase = sayHiToGrandma
+
+function sayHiToGrandma(string) {
+  string.toUpperCase() === uppercase
+  return "YES INDEED!"
+}
+
+function sayHiToGrandma(string) {
+  string === ("I love you, Grandma.")
+  return "I love you, too."
+}
+
+var string = string.toUpperCase
+
+function sayHiToGrandma(string) {
+  if(string.toLowerCase() === lowercase)
+  return "I can't hear you!"
+}
